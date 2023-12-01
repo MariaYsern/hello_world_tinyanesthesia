8be1b3a2fb3540cc5ff93038ef671e6d6993a17e
arnaumarin <79079324+arnaumarin@users.noreply.github.com>
2023-12-01 14:09:35 -0500

    update SleepModel + Classifier model

--- hello_world_tinyanesthesia.ino
+++ hello_world_tinyanesthesia.ino
@@ -31,16 +31,25 @@ namespace {
 tflite::ErrorReporter* error_reporter = nullptr;
 const tflite::Model* model = nullptr;
 tflite::MicroInterpreter* interpreter = nullptr;
-
-
 TfLiteTensor* input = nullptr;
 TfLiteTensor* output = nullptr;
-TfLiteTensor* output2 = nullptr;
 int inference_count = 0;
-
-constexpr int kTensorArenaSize = 150*1024;
+constexpr int kTensorArenaSize = 75*1024;
 uint8_t tensor_arena[kTensorArenaSize];
-}  // namespace
+}  
+
+namespace {
+tflite::ErrorReporter* error_reporter2 = nullptr;
+const tflite::Model* model2 = nullptr;
+tflite::MicroInterpreter* interpreter2 = nullptr;
+TfLiteTensor* input2 = nullptr;
+TfLiteTensor* output2 = nullptr;
+int inference_count2 = 0;
+
+constexpr int kTensorArenaSize2 = 65*1024;
+uint8_t tensor_arena2[kTensorArenaSize2];
+}  
+
 
 const int length_data = 200; //
 const int num_channels = 1;
@@ -52,7 +61,7 @@ int vector_data[length_data] = {
   72, 80, 70, 59, 54, -2, -47, -72, -49, -27, -49, -77, -83, -32, 22, 62, 90, 68, 16, -48, -47, -11, -13, -22, -10, 21, -28, -93, -73, -3, 65, 63, 17, -21, -34, -32, -33, 2, 45, 43, 39, 47,2,3,4,5,6,7,8,9,
 };
 
-// The name of this function is important for Arduino compatibility.
+
 void setup() {
   // Set up logging. Google style is to avoid globals or statics because of
   // lifetime uncertainty, but since this has a trivial destructor it's okay.
@@ -60,14 +69,16 @@ void setup() {
   static tflite::MicroErrorReporter micro_error_reporter;
   error_reporter = &micro_error_reporter;
 
+  static tflite::MicroErrorReporter micro_error_reporter2;
+  error_reporter2 = &micro_error_reporter2;
+
   // Map the model into a usable data structure. This doesn't involve any
   // copying or parsing, it's a very lightweight operation.
   model = tflite::GetModel(g_model);
-  if (model->version() != TFLITE_SCHEMA_VERSION) {
-    TF_LITE_REPORT_ERROR(error_reporter,
-                         "Model provided is schema version %d not equal "
-                         "to supported version %d.",
-                         model->version(), TFLITE_SCHEMA_VERSION);
+  model2 = tflite::GetModel(g_model2);
+
+  if (model->version() != TFLITE_SCHEMA_VERSION || model2->version() != TFLITE_SCHEMA_VERSION) {
+    TF_LITE_REPORT_ERROR(error_reporter, "Model version does not match Schema version");
     return;
   }
 
@@ -80,26 +91,37 @@ void setup() {
       model, resolver, tensor_arena, kTensorArenaSize, error_reporter);
   interpreter = &static_interpreter;
 
+  static tflite::MicroInterpreter static_interpreter2(
+      model2, resolver, tensor_arena2, kTensorArenaSize2, error_reporter2);
+  interpreter2 = &static_interpreter2;
+
   // Allocate memory from the tensor_arena for the model's tensors.
   TfLiteStatus allocate_status = interpreter->AllocateTensors();
   if (allocate_status != kTfLiteOk) {
     TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
     return;
   }
+  
+  TfLiteStatus allocate_status2 = interpreter2->AllocateTensors();
+  if (allocate_status2 != kTfLiteOk) {
+    TF_LITE_REPORT_ERROR(error_reporter, "for the second model AllocateTensors() failed");
+    return;
+  }
 
   // Obtain pointers to the model's input and output tensors.
   input = interpreter->input(0);
   output = interpreter->output(0);
-  output2 = interpreter->output(1);
+
+  input2 = interpreter2->input(0);
+  output2 = interpreter2->output(0);
 
   // Keep track of how many inferences we have performed.
   inference_count = 0;
+  inference_count2 = 0;
 }
 
 // The name of this function is important for Arduino compatibility.
 void loop() {
-  Serial.print("Number of dimensions input: ");
-  Serial.println(input->dims->size);
   Serial.print("Dim 1 size input: ");
   Serial.println(input->dims->data[0]);
   Serial.print("Dim 2 size inputs: ");
@@ -111,43 +133,65 @@ void loop() {
   Serial.print("Dim 2 size output: ");
   Serial.println(output->dims->data[1]);
 
+  Serial.println("#######################");
+  Serial.println("this is for second model :D");
+  Serial.print("Dim 1 size input: ");
+  Serial.println(input2->dims->data[0]);
+  Serial.print("Dim 2 size inputs: ");
+  Serial.println(input2->dims->data[1]);
+  delay(1000);
+  Serial.print("Number of dimensions output: ");
+  Serial.print("Dim 1 size output: ");
+  Serial.println(output2->dims->data[0]);
+  Serial.print("Dim 2 size output: ");
+  Serial.println(output2->dims->data[1]);
+  Serial.println("#######################");
+  Serial.println("starts the input");
 
+  for (int i = 0; i < 200; ++i) {
+    input->data.int8[i] = vector_data[i];
+    input2->data.int8[i] = vector_data[i];
+    Serial.print(input2->data.int8[i]);
+    Serial.print(", ");
+  }
+  Serial.println("this was the input");
+  
 
-
-
-  // Calculate an x value to feed into the model. We compare the current
-  // inference_count to the number of inferences per cycle to determine
-  // our position within the range of possible x values the model was
-  // trained on, and use this to calculate a value.
-  float position = static_cast<float>(inference_count) /
-                   static_cast<float>(kInferencesPerCycle);
-  float x = position * kXrange;
-
-  // Quantize the input from floating-point to integer
-  int8_t x_quantized[200] = vector_data / input->params.scale + input->params.zero_point;
-  // Place the quantized input in the model's input tensor
-  input->data.int8[200] = x_quantized;
-
-  // Run inference, and report any error
-  TfLiteStatus invoke_status = interpreter->Invoke();
-  if (invoke_status != kTfLiteOk) {
-    TF_LITE_REPORT_ERROR(error_reporter, "Invoke failed on x: %f\n",
-                         static_cast<double>(x));
+  // Run inference on model 1
+  if (interpreter->Invoke() != kTfLiteOk) {
+    TF_LITE_REPORT_ERROR(error_reporter, "Invoke failed for model 1");
     return;
   }
 
-  // Obtain the quantized output from model's output tensor
-  int8_t y_quantized[3] = output->data.int8[0];
-  // Dequantize the output from integer to floating-point
-  float y[3] = (y_quantized - output->params.zero_point) * output->params.scale;
+  // Process output from model 1
+  for (int i = 0; i < output->dims->data[1]; ++i) {
+    // Example: Print output value
+    Serial.print("Model 1 Output [");
+    Serial.print(i);
+    Serial.print("]: ");
+    Serial.println(output->data.int8[i]);
+  }
 
-  // Output the results. A custom HandleOutput function can be implemented
-  // for each supported hardware target.
-  HandleOutput(error_reporter, x, y);
+  // Run inference on model 2
+  if (interpreter2->Invoke() != kTfLiteOk) {
+    TF_LITE_REPORT_ERROR(error_reporter, "Invoke failed for model 2");
+    return;
+  }
 
-  // Increment the inference_counter, and reset it if we have reached
-  // the total number per cycle
-  inference_count += 1;
-  if (inference_count >= kInferencesPerCycle) inference_count = 0;
-  delay(10000);
+  // Process output from model 2
+  for (int i = 0; i < output2->dims->data[1]; ++i) {
+    // Example:
+    // Example: Print output value
+    Serial.print("Model 2 Output [");
+    Serial.print(i);
+    Serial.print("]: ");
+    Serial.println(output2->data.int8[i]);
+  }
+
+  // Add any additional logic or delays as needed
+  delay(1000); // Example delay
+
+
+
+
 }
